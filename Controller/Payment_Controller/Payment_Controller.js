de93b52831ef749640ba3721587066be8962a730
RnRukon <rukon.js@gmail.com>
2023-10-17 01:38:23 +0600

    api change

--- Controller/Payment_Controller/Payment_Controller.js
+++ Controller/Payment_Controller/Payment_Controller.js
@@ -1,7 +1,6 @@
-
 const SSLCommerzPayment = require('sslcommerz');
-const { v4: uuidv4 } = require('uuid');
-const { orderCollection } = require('../../MongoDB/MongoDBCollection');
+const {v4: uuidv4} = require('uuid');
+const {orderCollection} = require('../../MongoDB/MongoDBCollection');
 
 
 module.exports.paymentInit = async (req, res) => {
@@ -11,10 +10,10 @@ module.exports.paymentInit = async (req, res) => {
             currency: 'BDT',
             tran_id: uuidv4(),
 
-            success_url: 'https://evening-woodland-47343.herokuapp.com/api/v1/payment/success',
-            fail_url: 'https://evening-woodland-47343.herokuapp.com/api/v1/payment/fail',
-            cancel_url: 'https://evening-woodland-47343.herokuapp.com/api/v1/payment/cancel',
-            ipn_url: 'https://evening-woodland-47343.herokuapp.com/api/v1/payment/ipn',
+            success_url: 'https://online-clock-api.vercel.app/api/v1/payment/success',
+            fail_url: 'https://online-clock-api.vercel.app/api/v1/payment/fail',
+            cancel_url: 'https://online-clock-api.vercel.app/api/v1/payment/cancel',
+            ipn_url: 'https://online-clock-api.vercel.app/api/v1/payment/ipn',
             shipping_method: 'Courier',
             product_name: req.body.product_name,
             product_category: 'Electronic',
@@ -69,7 +68,7 @@ module.exports.paymentInit = async (req, res) => {
 
 module.exports.paymentSuccess = async (req, res) => {
     try {
-        const result = await orderCollection.updateOne({ tran_id: req.body.tran_id }, {
+        const result = await orderCollection.updateOne({tran_id: req.body.tran_id}, {
             $set: {
                 val_id: req.body.val_id
             }
@@ -82,7 +81,7 @@ module.exports.paymentSuccess = async (req, res) => {
 
 module.exports.paymentFail = async (req, res) => {
     try {
-        const result = await orderCollection.deleteOne({ tran_id: req.body.tran_id })
+        const result = await orderCollection.deleteOne({tran_id: req.body.tran_id})
         res.status(400).redirect('https://premier-pottery-retailer.web.app')
     } catch (error) {
         res.status(400).send("Server Error ♨_♨");
@@ -91,7 +90,7 @@ module.exports.paymentFail = async (req, res) => {
 
 module.exports.paymentCancel = async (req, res) => {
     try {
-        const result = await orderCollection.deleteOne({ tran_id: req.body.tran_id })
+        const result = await orderCollection.deleteOne({tran_id: req.body.tran_id})
         res.status(300).redirect('https://premier-pottery-retailer.web.app')
     } catch (error) {
         res.status(400).send("Server Error ♨_♨");
@@ -109,7 +108,7 @@ module.exports.paymentIpn = async (req, res) => {
 module.exports.paymentOrder = async (req, res) => {
     try {
         const id = req.params.tran_id;
-        const result = await orderCollection.findOne({ tran_id: id })
+        const result = await orderCollection.findOne({tran_id: id})
         res.status(200).json(result)
     } catch (error) {
         res.status(400).send("Server Error ♨_♨");
